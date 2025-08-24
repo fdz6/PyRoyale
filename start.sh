996fc324f7944813d733f0cfb34fef339101ecfc
fusiondevz <132731805+fdz6@users.noreply.github.com>
2025-08-24 14:20:03 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
-# PyRoyale v1.0 - Termux version
+#!/data/data/com.termux/files/usr/bin/sh
 cd "$(dirname "$0")"
+if ! command -v python >/dev/null 2>&1; then
+    pkg update -y
+    pkg install -y python
+fi
+python -m pip install --upgrade pip
 python src/Main.py
